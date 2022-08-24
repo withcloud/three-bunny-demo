9ade63a46306a5a1211bcb103a662d3b8fff010c
pengchihui1 <pengchihui1@gmail.com>
2022-08-24 10:06:32 +0800

    调整

--- js/controls/DeviceOrientationControls.js
+++ js/controls/DeviceOrientationControls.js
@@ -1,120 +1,113 @@
 import * as THREE from 'three';
-/**
- * @classdesc Device Orientation Control
- * @constructor
- * @external DeviceOrientationControls
- * @param {THREE.Object} object
- */
+const DeviceOrientationControls = function (object) {
 
-let beta = 0;
-let alpha = 0;
-let gamma = 0;
-let orient = 10;
-let speed_percent = 1 / 100;
-function DeviceOrientationControls(object) {
+  var scope = this;
 
-
-  const scope = this;
-  this.object = object;//被控制的摄像机。
+  this.object = object;
   this.object.rotation.reorder('YXZ');
+
   this.enabled = true;
-  this.deviceOrientation = {};//当前 deviceorientation 事件的对象
-  this.screenOrientation = 0;//相对于设备自然朝向的视口朝向，以角度表示（增量为90）。默认为0。
-  this.alphaOffset = 0; // radians 角偏移量，以弧度表示，默认为0。
-  this.initialOffset = null;
 
-  const onDeviceOrientationChangeEvent = function ({ alpha, beta, gamma }) {
-    if (scope.initialOffset === null) {
-      scope.initialOffset = alpha;
-    }
-    alpha = alpha - scope.initialOffset;
-    if (alpha < 0) alpha += 360;
-    scope.deviceOrientation = { alpha, beta, gamma };
+  this.deviceOrientation = {};
+  this.screenOrientation = 0;
+
+  this.horizontalOffset = 0; // degrees
+  this.verticalOffset = 0; // degrees
+
+  var onDeviceOrientationChangeEvent = function (event) {
+
+    scope.deviceOrientation = event;
+
+    if (event.alpha == null)
+      scope.deviceOrientation = { alpha: 90, beta: 90, gamma: 0 };
+
   };
 
-  const onScreenOrientationChangeEvent = function () {
+  var onScreenOrientationChangeEvent = function () {
+
     scope.screenOrientation = window.orientation || 0;
+
   };
 
-  const onRegisterEvent = function () {
-    window.addEventListener('orientationchange', onScreenOrientationChangeEvent, false);
-    window.addEventListener('deviceorientation', onDeviceOrientationChangeEvent, false);//手機執行
-  }.bind(this);
-
-  // 更新相机三维角度
   // The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
-  const setObjectQuaternion = function () {
-    const zee = new THREE.Vector3(0, 0, 0);
-    const euler = new THREE.Euler();
-    const q0 = new THREE.Quaternion();
-    const q1 = new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)); // - PI/2 around the x-axis
+
+  var setObjectQuaternion = function () {
+
+    var zee = new THREE.Vector3(0, 0, 1);
+
+    var euler = new THREE.Euler();
+
+    var q0 = new THREE.Quaternion();
+
+    var q1 = new THREE.Quaternion(- Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)); // - PI/2 around the x-axis
+
     return function (quaternion, alpha, beta, gamma, orient) {
-      euler.set(beta, alpha, -gamma, 'YXZ'); // 'ZXY' for the device, but 'YXZ' for us
-      quaternion.setFromEuler(euler); // orient the device 定位设备
-      quaternion.multiply(q1); // camera looks out the back of the device, not the top 相机从设备的背面看，而不是顶部
-      quaternion.multiply(q0.setFromAxisAngle(zee, -orient)); // adjust for screen orientation 调整屏幕方向
+
+      euler.set(beta, alpha, - gamma, 'YXZ'); // 'ZXY' for the device, but 'YXZ' for us
+
+      quaternion.setFromEuler(euler); // orient the device
+
+      quaternion.multiply(q1); // camera looks out the back of the device, not the top
+
+      quaternion.multiply(q0.setFromAxisAngle(zee, - orient)); // adjust for screen orientation
+
     };
+
   }();
 
   this.connect = function () {
+
     onScreenOrientationChangeEvent(); // run once on load
-    // iOS 13+
-    if (window.DeviceOrientationEvent !== undefined && typeof window.DeviceOrientationEvent.requestPermission === 'function') {
-      window.DeviceOrientationEvent.requestPermission().then(function (response) {
-        if (response == 'granted') {
-          onRegisterEvent();
-        }
-      }).catch(function (error) {
-        console.error('THREE.DeviceOrientationControls: Unable to use DeviceOrientation API:', error);
-      });
-    } else {
-      onRegisterEvent();
-    }
+
+    window.addEventListener('orientationchange', onScreenOrientationChangeEvent, false);
+    window.addEventListener('deviceorientation', onDeviceOrientationChangeEvent, false);
+
     scope.enabled = true;
+
   };
 
   this.disconnect = function () {
+
     window.removeEventListener('orientationchange', onScreenOrientationChangeEvent, false);
     window.removeEventListener('deviceorientation', onDeviceOrientationChangeEvent, false);
+
     scope.enabled = false;
-    scope.deviceOrientation = null;
-    scope.initialOffset = null;
+
   };
 
-  this.update = function () {
+  this.update = function (_horizontalOffset, _verticalOffset) {
 
     if (scope.enabled === false) return;
-    // 陀螺儀變化量
-    window.addEventListener("deviceorientation", (event) => {
 
-      // 移动设备水平放置时，绕X轴旋转的角度，数值为-180度到180度。
-      if (event?.beta) {
-        beta = Number(event?.beta.toFixed(1)) * speed_percent;
-      }
-      //  移动设备水平放置时，绕Y轴旋转的角度，数值为-90度到90度。
-      if (event?.gamma) {
-        gamma = Number(event?.gamma.toFixed(1)) * speed_percent;
-      }
-      // 移动设备水平放置时，绕z轴旋转的角度，数值为0度到360度。
-      if (event?.alpha) {
-        alpha = Number(event?.alpha.toFixed(1)) * speed_percent;
-      }
-      orient = scope.screenOrientation ? scope.screenOrientation : 0; // O
-      document.getElementById("demo_x").innerHTML = `x = ${beta?.toFixed(1)}`;
-      document.getElementById("demo_z").innerHTML = `z = ${alpha?.toFixed(1)}`;
-      document.getElementById("demo_y").innerHTML = `y = ${gamma?.toFixed(1)}`;
-      document.getElementById("demo_orient").innerHTML = `orient = ${orient?.toFixed(1)}`;
-      scope.deviceOrientation = { alpha, beta, gamma };
-      setObjectQuaternion(scope.object.quaternion, alpha, beta, gamma, orient);
-    });
+    if (_horizontalOffset != undefined) scope.horizontalOffset = _horizontalOffset;
+    if (_verticalOffset != undefined) scope.verticalOffset = _verticalOffset;
+
+    if (!scope.deviceOrientation) {
+      scope.deviceOrientation = { alpha: 90, beta: 90, gamma: 0 };
+      scope.screenOrientation = 0;
+    }
+    var device = scope.deviceOrientation;
+
+    var alpha = THREE.Math.degToRad(device.alpha); // Z
+    var beta = THREE.Math.degToRad(device.beta); // X'
+    var gamma = THREE.Math.degToRad(device.gamma); // Y''
+
+    var orient = THREE.Math.degToRad(scope.screenOrientation); // O
+
+    setObjectQuaternion(scope.object.quaternion, alpha, beta, gamma, orient);
+
+    scope.object.rotateX(THREE.Math.degToRad(scope.verticalOffset));
+    var axisY = new THREE.Vector3(0, 1, 0);
+    scope.object.rotateOnWorldAxis(axisY, THREE.Math.degToRad(scope.horizontalOffset));
   };
 
   this.dispose = function () {
+
     scope.disconnect();
+
   };
 
+  this.connect();
 };
-DeviceOrientationControls.prototype = Object.assign(Object.create(THREE.EventDispatcher.prototype), {
-  constructor: DeviceOrientationControls
-});
+
 export { DeviceOrientationControls };
